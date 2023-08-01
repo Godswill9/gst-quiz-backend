4b2e6093cdc0863a200751d7b778ee98cd82ab5d
godswill9 <ghycinth9@gmail.com>
2023-07-31 21:04:11 -0700

    new changes

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(
   cors({
     origin: [
       "http://127.0.0.1:5502",
+      "http://127.0.0.1:5500",
+      "http://127.0.0.1:5501",
       "http://localhost:5501",
       "http://localhost:3000",
     ],
