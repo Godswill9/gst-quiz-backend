713720ea812027854a1fe06ff393e7c7cfe889cb
godswill9 <ghycinth9@gmail.com>
2023-08-02 09:21:39 -0700

    cors update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
       "http://127.0.0.1:5501",
       "http://localhost:5501",
       "http://localhost:3000",
-      "https://admingst.netlify.app/",
+      "https://admingst.netlify.app",
       // "https://solvegst.onrender.com/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
