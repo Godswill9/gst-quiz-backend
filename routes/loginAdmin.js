60b5c43938ae5b9edd2c880dc32bbbce10dad2c0
godswill9 <ghycinth9@gmail.com>
2023-08-03 01:47:21 -0700

    new

--- routes/loginAdmin.js
+++ routes/loginAdmin.js
@@ -4,6 +4,9 @@ const { authenticate } = require("../config/verifyAdmin");
 const route = express.Router();
 
 route.post("/loginAdmin", login);
+route.get("/loginAdmin", (req, res) => {
+  res.send("login route");
+});
 
 route.post("/checkAdmin", authenticate);
 
